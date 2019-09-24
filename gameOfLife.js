0354d1426b46f570deddead01804871cb9890d37
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-09-24 17:36:59 +0100

    TDD out new cell state logic

---
+++ gameOfLife.js
@@ -0,0 +1,8 @@
+function newCellState(isCurrentlyAlive, numberOfNeighbours) {
+  if (!isCurrentlyAlive) {
+    return numberOfNeighbours === 3;
+  }
+  return numberOfNeighbours >= 2 && numberOfNeighbours < 4;
+}
+
+module.exports = { newCellState };
--- gameOfLife.spec.js
+++ gameOfLife.spec.js
@@ -1,3 +1,34 @@
-test('Test runs', () => {
+const { newCellState } = require("./gameOfLife");
 
+test("Any live cell with fewer than two live neighbours dies, as if by underpopulation.", () => {
+  expect(newCellState(true, 0)).toBe(false);
+  expect(newCellState(true, 1)).toBe(false);
+});
+
+test("Any live cell with two or three live neighbours lives on to the next generation.", () => {
+  expect(newCellState(true, 2)).toBe(true);
+  expect(newCellState(true, 3)).toBe(true);
+});
+
+test("Any live cell with more than three live neighbours dies, as if by overpopulation.", () => {
+  expect(newCellState(true, 4)).toBe(false);
+  expect(newCellState(true, 5)).toBe(false);
+  expect(newCellState(true, 6)).toBe(false);
+  expect(newCellState(true, 7)).toBe(false);
+  expect(newCellState(true, 8)).toBe(false);
+});
+
+test("Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.", () => {
+  // Dead with exactly three neighbours is 'born'
+  expect(newCellState(false, 3)).toBe(true);
+
+  // Dead with any other number of neighbours stays dead
+  expect(newCellState(false, 0)).toBe(false);
+  expect(newCellState(false, 1)).toBe(false);
+  expect(newCellState(false, 2)).toBe(false);
+  expect(newCellState(false, 4)).toBe(false);
+  expect(newCellState(false, 5)).toBe(false);
+  expect(newCellState(false, 6)).toBe(false);
+  expect(newCellState(false, 7)).toBe(false);
+  expect(newCellState(false, 8)).toBe(false);
 });
