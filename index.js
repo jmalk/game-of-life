fa6439e02f99e5046450ece9312eb9c6bce24d58
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-10-04 16:07:54 +0100

    Extract magic strings as consts

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const tick = require("./tick");
 const render = require("./render");
 const tickDuration = 1000;
+const liveGlyph = "•";
+const deadGlyph = "_";
 
 let state = [[false, false, false], [true, true, true], [false, false, false]];
 
 setInterval(function() {
   console.clear();
   state = tick(state);
-  console.log(render(state, "•", "_"));
+  console.log(render(state, liveGlyph, deadGlyph));
 }, tickDuration);
