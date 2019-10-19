7a201c82b943bbe39c36b1f0f4e7e87caeb86ada
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-10-19 22:00:21 +0100

    Add command-line arg to do random start

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const tick = require("./lib/tick");
 const render = require("./lib/render");
+const randomState = require("./lib/randomState");
+
 const tickDuration = 1000;
 const liveGlyph = "██";
 const deadGlyph = "  ";
 
-let state = [
+const args = process.argv.slice(2);
+const randomStart = args.includes("random");
+
+let defaultState = [
   [true, false, false, true, true],
   [true, false, false, true, true],
   [true, false, true, false, true],
   [true, false, false, true, true],
   [true, false, false, true, true]
 ];
+
+let state = randomStart ? randomState(20, 20) : defaultState;
 
 setInterval(function() {
   state = tick(state);
