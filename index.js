8387a252d408ebdb1a40e239b19f8ab00748baf6
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-10-19 22:36:04 +0100

    Let user override defaults

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const tick = require("./lib/tick");
 const render = require("./lib/render");
 const randomState = require("./lib/randomState");
-const {
-  initialState,
-  tickLength,
-  liveGlyph,
-  deadGlyph
-} = require("./default-config");
+const defaults = require("./default-config");
 
 const argv = require("yargs").argv;
-
-const randomStart = argv.random;
-
-let state = randomStart ? randomState(20, 20) : initialState;
+const tickLength = argv.tickLength || defaults.tickLength;
+const liveGlyph = argv.liveGlyph || defaults.liveGlyph;
+const deadGlyph = argv.deadGlyph || defaults.deadGlyph;
+let state = argv.random ? randomState(20, 20) : defaults.initialState;
 
 setInterval(function() {
   state = tick(state);
