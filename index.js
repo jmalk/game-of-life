a8122be1322401d78eeabaaacb11a198af7c315d
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-10-19 22:43:24 +0100

    Let user override width and height
    For random state only.

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const argv = require("yargs").argv;
 const tickLength = argv.tickLength || defaults.tickLength;
 const liveGlyph = argv.liveGlyph || defaults.liveGlyph;
 const deadGlyph = argv.deadGlyph || defaults.deadGlyph;
-let state = argv.random ? randomState(20, 20) : defaults.initialState;
+
+// These dimensions are only used for random initial state.
+// The default initial state has it's own dimensions, which don't need
+// specifying. They just are what they are.
+const width = argv.width || defaults.width;
+const height = argv.height || defaults.height;
+
+let state = argv.random ? randomState(height, width) : defaults.initialState;
 
 setInterval(function() {
   state = tick(state);
